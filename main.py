6fb18eba8843bb4fe5b156bcacdcabe724bacece
fursenko <roman.fursenko@gmail.com>
2022-11-26 19:46:22 +0700

    small reformating

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from logging.config import dictConfig
 from analysis import Analysis
 from logger_config import LOGGING_CONFIG
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dictConfig(LOGGING_CONFIG)
 
@@ -25,5 +19,3 @@ if __name__ == '__main__':
     print(res)
     database.stop()
     logging.info("finish main")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
