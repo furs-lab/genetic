fcb5f1c6d8b8f4a03544e9de120b18d0986b9df8
fursenko <roman.fursenko@gmail.com>
2022-11-26 10:43:44 +0700

    place all orm classes in separate module dbclasses.py

--- database.py
+++ database.py
@@ -2,37 +2,7 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import mapper, relationship, sessionmaker
 import logging
 from multipledispatch import dispatch
-
-
-class Clinics:
-    pass
-
-
-class Panels:
-    pass
-
-
-class PanelSet:
-    pass
-
-
-class Genes:
-    pass
-
-
-# engine = create_engine('mysql+pymysql://debian-sys-maint:UmX4EHHEbeT8Ad0F@localhost/genetic')
-engine = create_engine('mysql+pymysql://root:feromon@localhost/genetic')
-meta = MetaData(engine)
-
-clinics = Table('clinics', meta, autoload=True)
-panels = Table('panels', meta, autoload=True)
-panel_set = Table('panel_set', meta, autoload=True)
-genes = Table('genes', meta, autoload=True)
-
-mapper(Clinics, clinics)
-mapper(Panels, panels)
-mapper(PanelSet, panel_set)
-mapper(Genes, genes)
+from dbclasses import *
 
 dbsession = sessionmaker(bind=engine)
 session = dbsession()
---
+++ dbclasses.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import mapper
+
+class Clinics:
+    pass
+
+
+class Panels:
+    pass
+
+
+class PanelSet:
+    pass
+
+
+class Genes:
+    pass
+
+
+class Themes:
+    pass
+
+
+class ThemeSet:
+    pass
+
+
+# engine = create_engine('mysql+pymysql://debian-sys-maint:UmX4EHHEbeT8Ad0F@localhost/genetic')
+engine = create_engine('mysql+pymysql://root:feromon@localhost/genetic')
+meta = MetaData(engine)
+
+clinics = Table('clinics', meta, autoload=True)
+panels = Table('panels', meta, autoload=True)
+panel_set = Table('panel_set', meta, autoload=True)
+genes = Table('genes', meta, autoload=True)
+
+mapper(Clinics, clinics)
+mapper(Panels, panels)
+mapper(PanelSet, panel_set)
+mapper(Genes, genes)
