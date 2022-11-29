959936eaf2ec30a8bf585e5b12a472855f39790e
fursenko <roman.fursenko@gmail.com>
2022-11-29 17:25:31 +0700

    improve plotrisk.py

--- plotrisk.py
+++ plotrisk.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_risk(min, max, cur):
+def plot_risk(minr, maxr, cur):
     ngrid = 100
     plt.subplots()
-    x = np.linspace(min, max, ngrid)
+    x = np.linspace(minr, maxr, ngrid)
     y = np.zeros(ngrid)
     t = np.arange(ngrid)
-    print(int(cur*ngrid/(max-min)))
+    print(int(cur * ngrid / (maxr - minr)))
     plt.scatter(x, y, marker='s', c=t, s=100, cmap='RdYlGn_r')
     plt.scatter([cur], [0], marker='o',  s=500, facecolors='none', edgecolors='k', linewidths=3)
-    plt.text(max, -0.01, str(max), fontsize=18)
-    plt.text(min, -0.01, str(min), fontsize=18)
+    plt.text(maxr, -0.01, str(maxr), fontsize=18)
+    plt.text(minr, -0.01, str(minr), fontsize=18)
     plt.text(cur, 0.007, str(cur), fontsize=18)
     plt.axis('off')
     plt.show()
