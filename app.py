bb5e51662663abd5d42b497e5dd90c0c9cf1e247
fursenko <roman.fursenko@gmail.com>
2022-12-21 17:54:59 +0700

    in app limit the number of requests per minute

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, jsonify, request, abort
-from json import dump
+from datetime import datetime
 
 import main
 
+REQUESTS_PER_MINUTE = 3
+requests_number = 0
+previous_time = datetime.now()
+
 app = Flask(__name__)
+
+
+def can_responce():
+    global requests_number, previous_time
+    requests_number += 1
+    if (datetime.now() - previous_time).seconds > 60:
+        previous_time = datetime.now()
+        requests_number = 0
+    return requests_number <= REQUESTS_PER_MINUTE
 
 
 @app.route('/')
@@ -15,7 +28,10 @@ def index():
 def get_tasks():
     if not request.json:
         abort(400)
+    if not can_responce():
+        return jsonify({'error': f'limit {REQUESTS_PER_MINUTE} requests per minute'})
     dic = main.create_json_test(request.json)
+    dic = request.json
     return jsonify(dic)
 
 
